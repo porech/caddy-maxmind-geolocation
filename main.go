86ba1d95eb74e54a6b168a0bc5b0b04cfee70c8e
Alessandro Rinaldi <ale.rinaldi@criptalia.it>
2021-08-28 15:11:35 +0200

    Complete subdivisions and metro codes check

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"go.uber.org/zap"
 	"net"
 	"net/http"
+	"strconv"
 )
 
 // Interface guards
@@ -36,13 +37,45 @@ type MaxmindGeolocation struct {
 
 	// A list of countries that the filter will allow.
 	// If you specify this, you should not specify DenyCountries.
+	// If both are specified, DenyCountries will take precedence.
 	// All countries that are not in this list will be denied.
+	// You can specify the special value "UNK" to match unrecognized countries.
 	AllowCountries []string `json:"allow_countries"`
 
 	// A list of countries that the filter will deny.
 	// If you specify this, you should not specify AllowCountries.
+	// If both are specified, DenyCountries will take precedence.
 	// All countries that are not in this list will be allowed.
+	// You can specify the special value "UNK" to match unrecognized countries.
 	DenyCountries []string `json:"deny_countries"`
+
+	// A list of subdivisions that the filter will allow.
+	// If you specify this, you should not specify DenySubdivisions.
+	// If both are specified, DenySubdivisions will take precedence.
+	// All subdivisions that are not in this list will be denied.
+	// You can specify the special value "UNK" to match unrecognized subdivisions.
+	AllowSubdivisions []string `json:"allow_subdivisions"`
+
+	// A list of subdivisions that the filter will deny.
+	// If you specify this, you should not specify AllowSubdivisions.
+	// If both are specified, DenySubdivisions will take precedence.
+	// All subdivisions that are not in this list will be allowed.
+	// You can specify the special value "UNK" to match unrecognized subdivisions.
+	DenySubdivisions []string `json:"deny_subdivisions"`
+
+	// A list of metro codes that the filter will allow.
+	// If you specify this, you should not specify DenyMetroCodes.
+	// If both are specified, DenyMetroCodes will take precedence.
+	// All metro codes that are not in this list will be denied.
+	// You can specify the special value "UNK" to match unrecognized metro codes.
+	AllowMetroCodes []string `json:"allow_metro_codes"`
+
+	// A list of METRO CODES that the filter will deny.
+	// If you specify this, you should not specify AllowMetroCodes.
+	// If both are specified, DenyMetroCodes will take precedence.
+	// All metro codes that are not in this list will be allowed.
+	// You can specify the special value "UNK" to match unrecognized metro codes.
+	DenyMetroCodes []string `json:"deny_metro_codes"`
 
 	dbInst *maxminddb.Reader
 	logger *zap.Logger
@@ -74,6 +107,14 @@ func (m *MaxmindGeolocation) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 				current = 2
 			case "deny_countries":
 				current = 3
+			case "allow_subdivisions":
+				current = 4
+			case "deny_subdivisions":
+				current = 5
+			case "allow_metro_codes":
+				current = 6
+			case "deny_metro_codes":
+				current = 7
 			default:
 				switch current {
 				case 1:
@@ -83,6 +124,14 @@ func (m *MaxmindGeolocation) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 					m.AllowCountries = append(m.AllowCountries, d.Val())
 				case 3:
 					m.DenyCountries = append(m.DenyCountries, d.Val())
+				case 4:
+					m.AllowSubdivisions = append(m.AllowSubdivisions, d.Val())
+				case 5:
+					m.DenySubdivisions = append(m.DenySubdivisions, d.Val())
+				case 6:
+					m.AllowMetroCodes = append(m.AllowMetroCodes, d.Val())
+				case 7:
+					m.DenyMetroCodes = append(m.DenyMetroCodes, d.Val())
 				default:
 					return fmt.Errorf("unexpected config parameter %s", d.Val())
 				}
@@ -116,6 +165,29 @@ func (m *MaxmindGeolocation) Cleanup() error {
 	return nil
 }
 
+func (m *MaxmindGeolocation) checkAllowed(item string, allowedList []string, deniedList []string) bool {
+	if item == "" || item == "0" {
+		item = "UNK"
+	}
+	if len(deniedList) > 0 {
+		for _, i := range deniedList {
+			if i == item {
+				return false
+			}
+		}
+		return true
+	}
+	if len(allowedList) > 0 {
+		for _, i := range allowedList {
+			if i == item {
+				return true
+			}
+		}
+		return false
+	}
+	return true
+}
+
 func (m *MaxmindGeolocation) Match(r *http.Request) bool {
 
 	// If both the allow and deny fields are empty, let the request pass
@@ -145,23 +217,33 @@ func (m *MaxmindGeolocation) Match(r *http.Request) bool {
 		"Detected MaxMind data",
 		zap.String("ip", r.RemoteAddr),
 		zap.String("country", record.Country.ISOCode),
-		zap.String("subdivisions", record.Subdivisions[0].ISOCode),
+		zap.String("subdivisions", record.Subdivisions.CommaSeparatedISOCodes()),
 		zap.Int("metro_code", record.Location.MetroCode),
 	)
 
-	for _, denyCountry := range m.DenyCountries {
-		if denyCountry == record.Country.ISOCode {
+	if !m.checkAllowed(record.Country.ISOCode, m.AllowCountries, m.DenyCountries) {
+		m.logger.Debug("Country not allowed", zap.String("country", record.Country.ISOCode))
+		return false
+	}
+
+	if len(record.Subdivisions) > 0 {
+		for _, subdivision := range record.Subdivisions {
+			if !m.checkAllowed(subdivision.ISOCode, m.AllowSubdivisions, m.DenySubdivisions) {
+				m.logger.Debug("Subdivision not allowed", zap.String("subdivision", subdivision.ISOCode))
+				return false
+			}
+		}
+	} else {
+		if !m.checkAllowed("", m.AllowSubdivisions, m.DenySubdivisions) {
+			m.logger.Debug("Subdivision not allowed", zap.String("subdivision", ""))
 			return false
 		}
 	}
 
-	for _, allowCountry := range m.AllowCountries {
-		if allowCountry == record.Country.ISOCode {
-			return true
-		}
+	if !m.checkAllowed(strconv.Itoa(record.Location.MetroCode), m.AllowMetroCodes, m.DenyMetroCodes) {
+		m.logger.Debug("Metro code not allowed", zap.Int("metro_code", record.Location.MetroCode))
+		return false
 	}
 
-	// If there are no allowed countries, the default is true (pass if the country is not denied).
-	// If there are allowed countries, and they didn't match, return false.
-	return len(m.AllowCountries) < 1
+	return true
 }
